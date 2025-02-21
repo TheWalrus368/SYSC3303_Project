86eeb0043180ec3f2c79b8962ede30fb92f9b111
Daniel Esenwa <112897438+Hipocrypt@users.noreply.github.com>
2025-02-21 17:30:57 -0500

    Update Simulation.java
    Added javadocs

--- Iteration2/Simulation.java
+++ Iteration2/Simulation.java
@@ -1,9 +1,14 @@
+/**
+ * The Simulation class initializes and starts the fire incident simulation.
+ * It creates instances of the FireIncidentSubsystem, Scheduler, and DroneSubsystem,
+ * and starts them as separate threads to handle fire events and drone operations concurrently.
+ */
 public class Simulation {
     public static void main(String[] args) {
-        // CSV file path
+        // CSV file path containing fire event data
         String csvFilePath = "Iteration2/fire_events.csv";
 
-        // Initialize Scheduler
+       // Initialize the Scheduler, responsible for managing communication between subsystems
         Scheduler scheduler = new Scheduler();
 
         // Initialize FireIncidentSubsystem and DroneSubsystem with references to the Scheduler
@@ -15,6 +20,7 @@ public class Simulation {
         Thread schedulerThread = new Thread(scheduler);
         Thread droneThread = new Thread(droneSubsystem);
 
+        // Begin execution of subsystems
         fireIncidentThread.start();
         schedulerThread.start();
         droneThread.start();
