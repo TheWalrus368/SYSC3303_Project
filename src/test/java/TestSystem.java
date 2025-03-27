a770c52063565e72b9777a1ec921d6a290e5fd88
Tony <137113805+TonySitu@users.noreply.github.com>
2025-03-27 17:04:36 -0400

    removed some imports

--- src/test/java/TestSystem.java
+++ src/test/java/TestSystem.java
@@ -1,11 +1,6 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestSystem {
@@ -22,5 +17,10 @@ public class TestSystem {
         String csvFilePath = "src/main/java/fire_events.csv";
         FireIncidentSubsystem fireIncidentSubsystem = new FireIncidentSubsystem(csvFilePath);
         Thread fireIncidentThread = new Thread(fireIncidentSubsystem, "FIRE");
+    }
+
+    @Test
+    void test() {
+        assertTrue(true);
     }
 }
